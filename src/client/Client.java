aa6e28ea757959d0b989d4f41a0897169e1733eb
mdasc2 <achowbavc@gmail.com>
2018-06-05 05:00:08 -0700

    Basic usage of connecting to the unix2 server databases.

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package client;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -8,11 +21,26 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/** 
+/**
  *
- * @author John A. Torres
+ * @author jtorres
  */
 public class Client extends Application {
+    
+    
+    private static void doSshTunnel(String strSshUser, String strSshPassword, String strSshHost, int nSshPort,
+            String strRemoteHost, int nLocalPort, int nRemotePort) throws JSchException {
+        final JSch jsch = new JSch();
+        Session session = jsch.getSession(strSshUser, strSshHost, 22);
+        session.setPassword(strSshPassword);
+
+        final Properties config = new Properties();
+        config.put("StrictHostKeyChecking", "no");
+        session.setConfig(config);
+
+        session.connect();
+        session.setPortForwardingL(nLocalPort, strRemoteHost, nRemotePort);
+    }
     
     @Override
     public void start(Stage primaryStage) {
@@ -39,7 +67,60 @@ public class Client extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        
+        try {
+            String strSshUser = ""; // SSH loging username Whatever your user is
+            String strSshPassword = ""; // SSH login password Whatever your password is 
+            String strSshHost = "unix2.csc.calpoly.edu"; // hostname or ip or
+                                                            // SSH server
+            int nSshPort = 22; // remote SSH host port number
+            String strRemoteHost = "ambari-head.csc.calpoly.edu"; // hostname or
+                                                                    // ip of
+                                                                    // your
+                                                                    // database
+                                                                    // server
+            int nLocalPort = 3366; // local port number use to bind SSH tunnel
+            int nRemotePort = 3306; // remote port number of your database
+            String strDbUser = "shout"; // database loging username
+            String strDbPassword = "shout"; // database login password
+
+            Client.doSshTunnel(strSshUser, strSshPassword, strSshHost, nSshPort, strRemoteHost, nLocalPort,
+                    nRemotePort);
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:" + nLocalPort +
+                    "/shout?useLegacyDatetimeCode=false&serverTimezone=UTC", strDbUser,
+                    strDbPassword);
+            
+            Statement stmt = con.createStatement();
+            String sql;
+            sql = "SELECT * from business";
+            ResultSet rs = stmt.executeQuery(sql);
+            
+            while(rs.next()){
+                //Retrieve by column name
+                int id  = rs.getInt("bid");
+                String name = rs.getString("name");
+                String city = rs.getString("city");
+
+                //Display values
+                System.out.println("ID: " + id);
+                System.out.println("name: " + name);
+                System.out.println("city :" + city);
+
+             }
+            
+            con.close();
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            
+        }
+        System.out.println("LOL");
         launch(args);
     }
     
